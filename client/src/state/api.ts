5febdc68bbd30e91a1678f533b104dce3ff55849
Dushyant <dushyant.pareek.2002@gmail.com>
2024-09-16 17:27:58 +0530

    Added sidebar

---
+++ client/src/state/api.ts
@@ -0,0 +1,11 @@
+import { createApi, CreateApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import build from 'next/dist/build';
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BASE_URL }),
+  reducerPath: 'api',
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
